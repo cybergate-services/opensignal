f6b7b016ec72d584e2cf522651f3cb13917181f0
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-01 18:47:12 +0530

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -2,12 +2,13 @@ rm -rf ./.env
 PASSWORD=$(openssl rand -base64 16)
 ACCESS_KEY=$(openssl rand -base64 20)
 SECRET_KEY=$(openssl rand -base64 20)
-
-cat coturn/coturn.env > ./.coturn.env
-sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.coturn.env               
-
+     
 cat postgresql/postgresql.env > ./.postgresql.env
 sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.postgresql.env
+
+cat minio/minio.env > ./.minio.env
+sed -i -e "s/CHANGE_ACCESS_KEY/$ACCESS_KEY/g" ./.minio.env
+sed -i -e "s/CHANGE_SECRET_KEY/$SECRET_KEY/g" ./.minio.env
 
 cat letsencrypt/letsencrypt.env > ./.letsencrypt.env
 
