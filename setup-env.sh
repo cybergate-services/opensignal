ec64971f4b0425a84c1cc938a11b15831606f622
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-01 19:06:47 +0530

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -5,12 +5,11 @@ ACCESS_KEY=$(openssl rand -base64 20)
 SECRET_KEY=$(openssl rand -base64 20)
      
 cat postgresql/postgresql.env > ./.postgresql.env
-sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.postgresql.env
-
+cat letsencrypt/letsencrypt.env > ./.letsencrypt.env
 cat minio/minio.env > ./.minio.env
+
+sed -i -e "s/CHANGE_ME/$PASSWORD/g" ./.postgresql.env
 sed -i -e "s/CHANGE_ACCESS_KEY/$ACCESS_KEY/g" ./.minio.env
 sed -i -e "s/CHANGE_SECRET_KEY/$SECRET_KEY/g" ./.minio.env
-
-cat letsencrypt/letsencrypt.env > ./.letsencrypt.env
 
 echo "CHANGE_ME=${PASSWORD}" > ./.env
