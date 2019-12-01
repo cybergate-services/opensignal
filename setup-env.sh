fa39a0de2068f3f57bfe025361c113d71281a231
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-01 19:43:53 +0530

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 rm -rf ./.env
-PASSWORD=$(openssl rand -base64 16)
-ACCESS_KEY=$(uuidgen | sha1sum)
-SECRET_KEY=$(uuidgen | sha1sum)
-     
+PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
+ACCESS_KEY=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
+SECRET_KEY=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
+
 cat postgresql/postgresql.env > ./.postgresql.env
 cat letsencrypt/letsencrypt.env > ./.letsencrypt.env
 cat minio/minio.env > ./.minio.env
