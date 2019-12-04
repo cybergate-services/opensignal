0ee415be70ff7722fcb84160808b74982dff8715
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-04 22:39:42 +0530

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 rm -rf ./.env
+
 PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 ACCESS_KEY=$(openssl rand -hex 16)
 SECRET_KEY=$(openssl rand -hex 16)
 
-cat postgresql/postgresql.env > ./.postgresql.env
+cat postgresql/accountdb.env > ./.accountdb.env
+cat postgresql/messagedb.env > ./.messagedb.env
 cat letsencrypt/letsencrypt.env > ./.letsencrypt.env
 cat minio/minio.env > ./.minio.env
 
-sed -ie "s/CHANGE_ME/${PASSWORD}/g" ./.postgresql.env
+sed -ie "s/CHANGE_ME/${PASSWORD}/g" ./.accountdb.env
+sed -ie "s/CHANGE_ME/${PASSWORD}/g" ./.messagedb.env
+
 sed -ie "s/CHANGE_ACCESS_KEY/${ACCESS_KEY}/g" ./.minio.env
 sed -ie "s/CHANGE_SECRET_KEY/${SECRET_KEY}/g" ./.minio.env
 
